df34654232565e052c785e3eabfb7d476fadf31b
blindpro <noahbalboa34@gmail.com>
2026-04-06 07:08:52 -0500

    Fix: Rename process class to shell_process to fix name conflict with new nvgt process class.

---
+++ dep/sqlite3exts.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+int sqlite3_db_dump( sqlite3 *db, const char *zSchema, const char *zTable, int (*xCallback)(const char*,void*), void *pArg);
+int sqlite3_eval_init( sqlite3 *db,  char **pzErrMsg,  const sqlite3_api_routines *pApi);
+int sqlite3_spellfix_init(sqlite3 *db, char **pzErrMsg, const sqlite3_api_routines *pApi);
+#ifdef __cplusplus
+}
+#endif
